2afb42cbf35ee68bd67891cb01e19420652b1701
Alex Pearson <alex@alexpear.com>
2017-12-04 23:01:09 -0600

    Fix id mismatches

--- index.js
+++ index.js
@@ -1,87 +1,74 @@
-var faker = require('faker');
-var _ = require('lodash');
+var faker = require( "faker" );
+var _ = require( "lodash/fp" );
 
 var books = [
-  {
-      "name": "Adorable Puppies",
-      "author": "Binkey McGillocuddy",
-      "picture": "http://wallpapercave.com/wp/N2lOzO4.jpg",
-      "price": 9.95,
-      "sellingPoints": [
-          "Pugs Rule",
-          "Pugs are Sweet",
-          "Everybody Loves Pugs"
-      ]
-  },
-  {
-      "name": "Granite City Soul Suckers",
-      "author": "Bela Lugosi",
-      "picture": "http://medias.photodeck.com/abc3ec2e-e46d-11e0-8813-0175b4d176d6/100514_IND_003_xgaplus.jpg",
-      "price": 2.00,
-      "sellingPoints": [
-          "If you Haven't Been, Don't Go",
-          "Get Mugged Fast",
-          "Steal Your Car"
-      ]
-  },
-  {
-      "name": "JavaScript is SAVVY",
-      "author": "Benevolent Dictator",
-      "picture": "https://www.codementor.io/assets/page_img/learn-javascript.png",
-      "price": 45.99,
-      "sellingPoints": [
-          "Learn JavaScript",
-          "Survive Your First ReFactor",
-          "Free Coffee"
-      ]
-  },
-  {
-      "name": " Monty Python (the Expurgated Version)",
-      "author": "Noel HamWrapper",
-      "picture": "http://media.topito.com/wp-content/uploads/2014/10/python1.jpg",
-      "price": 30.00,
-      "sellingPoints": [
-          "Toupee Spam Muppet",
-          "Decal Slide Off My Car",
-          "Whatever You Can Steal"
-      ]
-  },
-  {
-      "name": " Monty Python 2: Electric Boogaloooo",
-      "author": "Noel HamWrapper",
-      "picture": "http://media.topito.com/wp-content/uploads/2014/10/python1.jpg",
-      "price": 50.00,
-      "sellingPoints": [
-          "Toupee Spam Muppet",
-          "Decal Slide Off My Car",
-          "Whatever You Can Steal",
-          "Look, another selling point!"
-      ]
-  }
+    {
+        "type": "book",
+        "id": 1,
+        "name": "The Desire Map",
+        "author": "Danielle LaPorte",
+        "pictureUrl": "https://images-na.ssl-images-amazon.com/images/I/41BhS87QXrL._SX258_BO1,204,203,200_.jpg",
+        "price": 6.32,
+        "sellingPoints": [
+            "Self Help",
+            "Inspiration",
+            "Coding"
+        ]
+    },
+    {
+        "type": "book",
+        "id": 2,
+        "name": "Savvy Coding For Dummies",
+        "author": "Alex Pearson",
+        "pictureUrl": "https://images-na.ssl-images-amazon.com/images/I/51q-YHB62GL._SX258_BO1,204,203,200_.jpg",
+        "price": 1799.00,
+        "sellingPoints": [
+            "Tech Help",
+            "Web Design",
+            "Coding"
+        ]
+    },
+    {
+        "type": "book",
+        "id": 3,
+        "name": "The Monster at the End of This Book",
+        "author": "Jon Stone",
+        "pictureUrl": "http://smollin.com/michael/tmonstr/mon001.jpg",
+        "price": 6.32,
+        "sellingPoints": [
+            "Timeless",
+            "Interactive",
+            "Coding"
+        ]
+    }
 ];
 
 function generateSellingPoints(){
-  return _.range(3)
-    .fill('')
-    .map(() => faker.lorem.sentence())
+    return _.pipe(
+        _.range,
+        _.fill( "" ),
+        _.map( () => faker.lorem.sentence() )
+    )( 3 );
 }
 
-function generateItems(type){
-  return _.range(20)
-    .map(id => (
-      {
-        type,
-        "id": id + 1,
-        "name": faker.lorem.sentence(),
-        "author": faker.fake("{{name.firstName}} {{name.lastName}}"),
-        "picture": faker.image.imageUrl(),
-        "price": faker.commerce.price(),
-        "sellingPoints": generateSellingPoints()
-      }
-    ));
+function generateItems( type ){
+    return _.pipe(
+        _.range,
+        _.map( ( id ) => (
+            {
+                type,
+                "id": id + books.length,
+                "name": faker.lorem.sentence(),
+                "author": faker.fake( "{{name.firstName}} {{name.lastName}}" ),
+                "picture": faker.image.imageUrl(),
+                "price": faker.commerce.price(),
+                "sellingPoints": generateSellingPoints()
+            }
+        ) )
+    )( 20 );
 }
 
-module.exports = () => ({
-  "books": [...books, ...generateItems('book')],
-  "music": generateItems('music')
-});
+module.exports = () => ( {
+    "books": [ ...books, ...generateItems( "book" ) ],
+    "music": generateItems( "music" )
+} );
