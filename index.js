4c9478a38ace5184d2d0e6c1aa12ac9542b1e227
Alex Pearson <alex@alexpear.com>
2017-12-07 15:31:18 -0600

    Change pictureUrl -> picture

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var books = [
         "id": 1,
         "name": "The Desire Map",
         "author": "Danielle LaPorte",
-        "pictureUrl": "https://images-na.ssl-images-amazon.com/images/I/41BhS87QXrL._SX258_BO1,204,203,200_.jpg",
+        "picture": "https://images-na.ssl-images-amazon.com/images/I/41BhS87QXrL._SX258_BO1,204,203,200_.jpg",
         "price": 6.32,
         "sellingPoints": [
             "Self Help",
@@ -20,7 +20,7 @@ var books = [
         "id": 2,
         "name": "Savvy Coding For Dummies",
         "author": "Alex Pearson",
-        "pictureUrl": "https://images-na.ssl-images-amazon.com/images/I/51q-YHB62GL._SX258_BO1,204,203,200_.jpg",
+        "picture": "https://images-na.ssl-images-amazon.com/images/I/51q-YHB62GL._SX258_BO1,204,203,200_.jpg",
         "price": 1799.00,
         "sellingPoints": [
             "Tech Help",
@@ -33,7 +33,7 @@ var books = [
         "id": 3,
         "name": "The Monster at the End of This Book",
         "author": "Jon Stone",
-        "pictureUrl": "http://smollin.com/michael/tmonstr/mon001.jpg",
+        "picture": "http://smollin.com/michael/tmonstr/mon001.jpg",
         "price": 6.32,
         "sellingPoints": [
             "Timeless",
